76b28965aa3ba0d66e2b05e6e6058897df0a45e5
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2025-01-18 12:36:52 -0500

    chore: Update document retrieval to return undefined for unauthenticated users, add Skeleton component for loading state, and enhance Home page layout with conditional rendering

--- app/header.tsx
+++ app/header.tsx
@@ -2,20 +2,28 @@ import { ModeToggle } from "@/components/ui/theme-toggle";
 
 import Image from "next/image";
 import { HeaderActions } from "./header-actions";
+import Link from "next/link";
 
 export function Header() {
   return (
     <div className="bg-slate-900 py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-4 text-2xl">
-          <Image
-            src="/Brain-Archive-.png"
-            alt="Brain-Archive"
-            width={50}
-            height={50}
-            className="rounded"
-          />
-          BRAIN ARCHIVE
+        <div className="flex gap-12 item-center">
+          <Link href="/" className="flex items-center gap-4 text-2xl">
+            <Image
+              src="/Brain-Archive-.png"
+              alt="Brain-Archive"
+              width={50}
+              height={50}
+              className="rounded"
+            />
+            BRAIN ARCHIVE
+          </Link>
+          <nav className="flex items-center">
+            <Link href="/" className="hover:text-slate-300">
+              Documents
+            </Link>
+          </nav>
         </div>
         <div className="flex gap-4 items-center">
           <ModeToggle />
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { useQuery } from "convex/react";
 
 import { DocumentCard } from "./document-card";
 import CreateDocumentButton from "./upload-document-button";
+import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
 
 export default function Home() {
   const documents = useQuery(api.documents.getDocuments);
@@ -14,11 +17,40 @@ export default function Home() {
         <h1 className="text-4xl font-bold">My Documents</h1>
         <CreateDocumentButton />
       </div>
-      <div className="grid grid-cols-4 gap-8">
-        {documents?.map((doc, index) => (
-          <DocumentCard key={index} document={doc} />
-        ))}
-      </div>
+      {!documents && (
+        <div className="grid grid-cols-4 gap-8">
+          {new Array(8).fill(null).map((_, index) => (
+            <Card className="h-[200px] p-6 flex flex-col justify-between">
+              <Skeleton className="h-6 w-full" />
+
+              <Skeleton className="h-6 w-full" />
+              <Skeleton className="h-6 w-full" />
+              <Skeleton className="h-8 w-24" />
+            </Card>
+          ))}
+        </div>
+      )}
+
+      {documents && documents.length === 0 && (
+        <div className="flex flex-col justify-center items-center space-y-8">
+          <Image
+            src="/upload-document.svg"
+            width={200}
+            height={200}
+            alt="Upload Document"
+          />
+          <h2 className="text-2xl font-semibold">You have no documents.</h2>
+          <CreateDocumentButton />
+        </div>
+      )}
+
+      {documents && documents.length > 0 && (
+        <div className="grid grid-cols-4 gap-8">
+          {documents?.map((doc, index) => (
+            <DocumentCard key={index} document={doc} />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
